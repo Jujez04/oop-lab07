bc01c97e4309e4650a1e77f21d2022fc12584396
Justin Carideo <justin.carideo@studio.unibo.it>
2024-11-10 17:18:28 +0100

    Corrected enum from a translation error

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,6 +1,8 @@
 package it.unibo.nestedenum;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * Implementation of {@link MonthSorter}.
@@ -22,8 +24,8 @@ public final class MonthSorterNested implements MonthSorter {
         AUGUST("august", LONG_MONTH),
         SEPTEMBER("september", USUAL_MONTH),
         OCTOBER("october", LONG_MONTH),
-        NOVEMBER("november", LONG_MONTH),
-        DICEMBER("dicember", LONG_MONTH);
+        NOVEMBER("november", USUAL_MONTH),
+        DECEMBER("december", LONG_MONTH);
 
         
         private int days;
@@ -35,12 +37,19 @@ public final class MonthSorterNested implements MonthSorter {
         }
 
         static Month fromString(String name) {
+            List<Month> matches = new ArrayList<>();
+
             for (Month i : Month.values()) {
-                if ( i.name.equals(name.toLowerCase())) {
-                    return i;
+                if ( i.name.startsWith(name.toLowerCase())) {
+                    matches.add(i);
                 }
             }
-            throw new IllegalArgumentException();
+
+            if(matches.isEmpty() || matches.size() > 1){
+                throw new IllegalArgumentException();
+            }
+
+            return matches.get(0);
         }
     };
         
@@ -56,8 +65,8 @@ public final class MonthSorterNested implements MonthSorter {
 
     private static class SortByDays implements Comparator<String>{
         @Override
-        public int compare(String o1, String o2) {
-            return Month.fromString(o1).days - Month.fromString(o2).days;
+        public int compare(String o1, String o2) { 
+            return Month.fromString(o1).days - Month.fromString(o2).days;  
         }
     }
 
@@ -69,3 +78,4 @@ public final class MonthSorterNested implements MonthSorter {
             
     }
 }
+
